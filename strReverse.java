a8992bce5a0eb4091902a86c0e58a737ef82ce45
saratchetri <48964375+saratchetri@users.noreply.github.com>
2019-03-27 00:57:45 +0530

    String reverse

---
+++ strReverse.java
@@ -0,0 +1,31 @@
+
+public class strReverse {
+
+	public static void main(String[] args) {
+		
+		strReverse.a("manoj");
+		System.out.println("\n.......");
+		strReverse.a("sarat");
+		
+		// TODO Auto-generated method stub
+		//string strA;
+		//String strA="manoj";
+//		
+//		for (int i=strA.length()-1;i>=0;i--)
+//		{
+//		System.out.print(strA.charAt(i));
+//		}
+		
+		
+}
+	
+	public static void a(String a1)
+	{
+		for (int i=a1.length()-1;i>=0;i--)
+		{
+		System.out.print(a1.charAt(i));
+		}
+		//return 0
+	}
+	
+}
