25928e7884f075b3be7568c1662b06f09e0a38e5
saratchetri <48964375+saratchetri@users.noreply.github.com>
2020-01-09 01:32:20 +0530

    Passing argument as 'SIX'

---
+++ Print1to10.java
@@ -0,0 +1,9 @@
+class Print1to10{
+
+    public static void main(String args[]){
+        for (int i=1;i<=20;i++){
+            System.out.print(i);
+            System.out.print("#");
+        }   
+    }
+}
